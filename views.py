79073615631ffb0ca359fe9cfee79835e80cea26
degustaf <degustaf.gmail.com>
2015-09-29 23:12:04 -0400

    Created initial version of page to remove words to make game.

--- views.py
+++ views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, render_to_response
 from django.http import Http404
 from django.template.context_processors import csrf
+from .models import Mad_Lib
 
 def Word_Madness_Index(request):
     return render(request, 'Word_Madness/index.html')
@@ -10,7 +11,14 @@ def Create_Game(request):
     if request.method == 'GET':
         return render(request, 'Word_Madness/create_story.html')
     if request.method == 'POST':
-        c={}
-        c.update(csrf(request))
-        return render_to_response(request, 'Word_Madness/create_story.html', c)
+        params = dict(request.POST)
+        if 'Words' in params:
+            pass
+        else:
+            print(params)
+            game = Mad_Lib(title=params['title'], text=params['body'])
+            game.save()
+            print(game.id)
+            params['id'] = game.id
+            return render(request, 'Word_Madness/remove_words.html', params)
     raise Http404("Page Not Found")

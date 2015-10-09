f3aae85b44d91ed9d2f94c5669f79796fca2a42a
degustaf <degustaf.gmail.com>
2015-10-08 23:03:21 -0400

    On click event fires on page load

--- static/Word_Madness/js/remove_words.js
+++ static/Word_Madness/js/remove_words.js
@@ -1,17 +1,25 @@
-$document.ready(function() {console.log("I'm Ready");})
 
-$(".story").on("click", function(e) {
-    s = window.getSelection();
-    var range = s.getRangeAt(0);
-    var node = s.anchorNode;
-    while (range.toString().indexOf(' ') != 0) {
-        range.setStart(node, (range.startOffset - 1));
-    }
-    range.setStart(node, range.startOffset + 1);
-    do {
-        range.setEnd(node, range.endOffset + 1);
+$( document ).ready( console.log("Page is ready"));
 
-    } while (range.toString().indexOf(' ') == -1 && range.toString().trim() != '' && range.endOffset < node.length);
-    var str = range.toString().trim();
-    alert(str);
-});
+$( document ).ready(
+    $("#story").on( "click", console.log("I've been clicked")))
+        
+        
+function select_word() {
+        console.log("I'm in");
+        var s = window.getSelection();
+        var range = s.getRangeAt(0);
+        var node = s.anchorNode;
+        while (range.toString().indexOf(' ') != 0) {
+            range.setStart(node, (range.startOffset - 1));
+        }
+        range.setStart(node, range.startOffset + 1);
+        do {
+            range.setEnd(node, range.endOffset + 1);
+
+        } while (range.toString().indexOf(' ') == -1 && range.toString().trim() != '' && range.endOffset < node.length);
+        var str = range.toString().trim();
+        alert(str);
+}
+
+console.log("I'm out");

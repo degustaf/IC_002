a0c3ba49592160c34a66492866f06c01f0da0117
degustaf <degustaf.gmail.com>
2015-10-10 12:37:15 -0400

    On click adds tag w/ class

--- static/Word_Madness/js/remove_words.js
+++ static/Word_Madness/js/remove_words.js
@@ -1,28 +1,34 @@
-
 function select_word() {
-        console.log("I'm in");
-        var s = window.getSelection();
-        var range = s.getRangeAt(0);
-        var node = s.anchorNode;
-        while (range.toString().indexOf(' ') != 0) {
-            range.setStart(node, (range.startOffset - 1));
-        }
-        range.setStart(node, range.startOffset + 1);
-        do {
-            range.setEnd(node, range.endOffset + 1);
+    var s = window.getSelection();
+    var range = s.getRangeAt(0);
+    var node = s.anchorNode;
 
-        } while (range.toString().indexOf(' ') == -1 && range.toString().trim() != '' && range.endOffset < node.length);
-        var str = range.toString().trim();
-        alert(str);
+    while (range.toString().indexOf(' ') != 0) {
+        range.setStart(node, (range.startOffset - 1));
+    }
+    range.setStart(node, range.startOffset + 1);
+
+    do {
+        range.setEnd(node, range.endOffset + 1);
+    } while (range.toString().indexOf(' ') == -1 && 
+            range.toString().trim() != '' && range.endOffset < node.length);
+    if (range.toString().indexOf(' ') != -1) {
+        range.setEnd(node, range.endOffset - 1);
+    }
+
+    return range;
+}
+
+function a() {
+    var range = select_word();
+    var str = range.toString().trim();
+    var newnode = document.createElement("a");
+    newnode.className = "blank_word";
+    range.surroundContents(newnode);
 }
 
 $(document).ready(function() {
-    console.log("Page is ready");
-
     $("#story").on( "click", function(){
-        console.log("I've been clicked");
-        select_word();
+        a();
     });
-        
-    console.log("I'm out");
 });
